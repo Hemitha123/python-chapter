5e2dea97ce86360c7443e4b407be50b948ba7e8e
Hemitha123 <hemithatech@gmail.com>
2025-03-19 19:53:25 +0530

    add new changes

---
+++ dictionary.py
@@ -0,0 +1,28 @@
+#   dictionary 
+
+marks = {
+    "harry":100,
+    "shubham":90,
+    "rohan":23
+}
+print(marks,type(marks)) 
+
+print(marks["shubham"])
+
+# dictionary methods 
+marks = {
+    "harry":100,
+    "shubham":90,
+    "rohan":23,
+    0:"harry"
+}
+print(marks.items()) 
+print(marks.keys())
+print(marks.values())
+marks.update({"harry":99,"rehuka":47})
+print(marks.get("rohan"))
+print(marks)
+
+# difference
+print(marks["shubham"])  #prints None
+print(marks.get("rohan"))   #returns error
