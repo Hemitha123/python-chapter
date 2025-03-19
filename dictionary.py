30dffa3db49e2cee65aa040394172cc319f0ac91
Hemitha123 <hemithatech@gmail.com>
2025-03-19 20:38:50 +0530

    add new changes

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,5 @@
+d={}  #empty dictionary
+
 #   dictionary 
 
 marks = {
---
+++ sets.py
@@ -0,0 +1,15 @@
+# s={1,3,55,765}  
+# print(s)
+
+# #  e=set()  # don't use s={} as it will create an empty dictionary
+
+# sets methods 
+
+s={1,3,5,5,765,"harry"}
+print(s, type(s))
+s.add(635)
+s.remove(5)
+s.pop()
+print(s,type(s))
+
+
