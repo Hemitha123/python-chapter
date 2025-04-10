d69cf92e821eebd0fbca8e37ab730bf5d66b5ef8
Hemitha123 <hemithatech@gmail.com>
2025-04-10 11:26:22 +0530

    add new changes

--- condionals.py
+++ condionals.py
@@ -1,7 +1,13 @@
+# If-elif-else ladder
 a=int(input("enter the age :"))
 if(a>=18):
     print("you are above the age of consent")
     print("good for you")
+
+elif(a<0):
+    print("you are entering the invalid age")
+elif(a==0):
+    print("you are entering a invalid age")
 else:
     print("you are below the age of consent")
 print("end of the program")
